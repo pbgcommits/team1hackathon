22d6166cab6467217cca4046eec678ba46be698b
Patrick Barton Grace <132808103+Tuthar@users.noreply.github.com>
2024-04-05 14:55:47 +1100

    Set up template and implemented basis for index.html, subjects.html, timetable.html

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/index.html")
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/subjects")
+def subjects():
+    return render_template("subjects.html")
+
+@app.route("/timetable")
+def timetable():
+    return render_template("timetable.html")
