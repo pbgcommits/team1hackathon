d08a2e3f2752c12f2daefbe7e40e031789f48da4
Patrick Barton Grace <132808103+Tuthar@users.noreply.github.com>
2024-04-05 18:21:47 +1100

    Setting up subjects page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -7,10 +8,35 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# Test data
+numSubjects = 4
+listOfSubjects = []
+tmpImageURI = "img/unimelblogo.png"
+for i in range(1, numSubjects + 1):
+    subject = "Subject " + str(i)
+    listOfSubjects.append({
+        "name": subject, 
+        "code": "COMP1000" + str(i), 
+        "teachingPeriod": {
+            "year": 2024, "semester": 1
+            },
+        "imgURI": tmpImageURI
+        })
+
 @app.route("/subjects")
 def subjects():
-    return render_template("subjects.html")
+    return render_template("subjects.html", numSubjects=numSubjects, subjects=listOfSubjects)
 
 @app.route("/timetable")
 def timetable():
     return render_template("timetable.html")
+
+@app.route("/subject/<subjectName>")
+def subject(subjectName):
+    subjectIndex = -1
+    for i in range(numSubjects):
+        if listOfSubjects[i]["name"] == subjectName:
+            subjectIndex = i
+            break
+    print(i)
+    return render_template("subject.html", subject=listOfSubjects[i])
