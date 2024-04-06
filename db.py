fb1e6cff9addb98991bb9ec7975a8c9157b342d4
Jacklyn Peng <pengchingyi@gmail.com>
2024-04-06 15:25:32 +1100

    database

--- db.py
+++ db.py
@@ -1,9 +1,8 @@
 from flask_pymongo import pymongo
+from secret_key import CONNECTION_STRING
+import pandas as pd
 
-
-CONNECTION_STRING = "mongodb+srv://jacklyn:ss0806449@cluster0.pip93yq.mongodb.net/?retryWrites=true&w=majority"
-
-
+ 
 # Create user that sends/receives requests
 client = pymongo.MongoClient(CONNECTION_STRING)
 
@@ -15,3 +14,10 @@ db = client.get_database("flask_mongodb_recipes")
 # Create a collection (JSON format dict) within the database
 collection = pymongo.collection.Collection(db, "collection")
 
+
+# 从CSV文件中读取数据
+data = pd.read_csv('/Users/jacklyn/Desktop/flask/appendix.csv')
+
+# 将数据转换为字典格式并插入到MongoDB集合中
+data_dict = data.to_dict(orient='records')
+collection.insert_many(data_dict)
